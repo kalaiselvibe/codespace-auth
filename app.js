802b3173463a7a218ac6ff601db53aeb27d5be48
kalaiselvi <119026993+kalaiselvibe@users.noreply.github.com>
2023-02-20 15:41:56 +0000

    add

--- app.js
+++ app.js
@@ -1,10 +1,104 @@
 require('dotenv').config()
 require("./database/database").connect()
-const express =require('express')
+const user = require('./model/user')
+const express = require('express')
+const bycript =require('bcryptjs')
+const jwt =require('jsonwebtoken')
+const cookieParser =require('cookies-parser')
 
-const app= express()
+const app =express()
+app.use(express.json())
+app.use(cookieParser())
 
-app.get("/",(req,res)=>{
+
+app/get("/",(req,res)=>{
     res.send("<h1>server is working</h1>")
 })
-module.exports=app
+app.post("/register",async(req,res) =>{
+    try {
+        // get all data from body
+        const {firstname,lastname,email,password}=req.body
+        // all the data should exists
+        if(!(firstname && lastname && email && password)){
+            res.status(400).send('All fields are compulsory')
+        }
+        //check if user already exists
+const  existingUser = await user.findOne({email })
+if(existingUser){
+    res.status(401).send('User already exists with this email')
+}
+
+
+        // encrypt the password
+const myEncPassword = await bycript.hash(password, 10)
+
+        // save the user in db
+       const user= await user.create({
+            firstname:firstname,
+            lastname:lastname,
+            email:email,
+            password: myEncPassword
+        })
+        // generate token for user and send it
+        const token =jwt.sign(
+            {id: user._id, email},
+            'shhhh' ,//process.env.jwtsecret
+            {
+                expiresIn:"2hr"
+            }
+        );
+user.token = token
+user.password = undefined
+
+res.status(201).json(user)
+
+    } catch (error)
+{
+    console.log(error);
+} }
+    )
+    app.post('/login', async (req,res)=>{
+        try{
+            //get all data from frontend
+            const {email,password}=req.body
+            //validation
+            if(!(email && password)){
+                res.sendStatus(400).sendStatus('sent all data')
+            }
+            //find user in db
+            const user= await user.findOne({email})
+            //assignment - if user is not there, then what?
+            //match the password
+            if (user && (await bycript.compare(password, user.password))){
+            jwt.sign(
+{id: user._id},
+'shhhhh',
+{
+    expiresIn: "2hr"
+}
+            );
+
+          user.token =token
+          user.password = undefined  
+          // send token user cookie  
+          // cookies section
+
+const options ={
+    expires:new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    httpOnly:true 
+};
+res.status(200).cookie("token",token,options).json({
+    success:true,
+    token,
+    user
+})
+            
+        }
+            //send a token
+        
+        } catch(error){
+            console.log(error);
+        }
+
+    })
+module.exports = app
--- database/database.js
+++ database/database.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose")
-const MONGODB_URL=process.env
+const {MONGODB_URL}=process.env
 exports.connect =()=>{
+    console.log(typeof MONGODB_URL);
     mongoose.connect(MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
+       
     })
-    .then()
+    .then(
+        console.log(`DB connected successfully`)
+    )
     .catch((error)=>{
         console.log('DB connection FAiled');
         console.log(error);
