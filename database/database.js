3460681ce642aea3b9a2ac60bf1a5281706becd5
kalaiselvi <119026993+kalaiselvibe@users.noreply.github.com>
2023-02-20 14:49:07 +0000

    add

--- database/database.js
+++ database/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-const MONGODB_URL=process.envexport.connect =()=>{
+const MONGODB_URL=process.env
+exports.connect =()=>{
     mongoose.connect(MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
